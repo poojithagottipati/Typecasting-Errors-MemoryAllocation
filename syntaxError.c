ad2cde94d526c6299b72587eb29bf241791cd430
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-04-08 16:38:46 +0530

    Create syntaxError.c

---
+++ syntaxError.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!")
+    return 0;
+}
+
+/*
+main.c: In function ‘main’:
+main.c:4:28: error: expected ‘;’ before ‘return’
+    4 |     printf("Hello, world!")
+      |                            ^
+      |                            ;
+    5 |     return 0;
+      |     ~~~~~~    
+*/
