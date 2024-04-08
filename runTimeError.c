b836b06e2566975f71cfef29688c550a7339311b
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-04-08 16:40:27 +0530

    Create runTimeError.c

---
+++ runTimeError.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 6, b = 0, result;
+    result = a / b; 
+    printf("Result: %d\n", result);
+    return 0;
+}
+//infinite result gives a runtime error
